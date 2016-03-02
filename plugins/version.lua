c61a2bb94a6fedb0b3f302819486a6f8873a459a
Tele-Fox <ehsan.gol80@yahoo.com>
2016-03-02 16:06:42 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,14 +14,14 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
+  return 'TeleFOX V1 Supergroups\nAn a powefull Bot   sudos:   telegram.me/Ehsan_Fox      telegram.me/tofaniyam    admins:    telegram.me/ali6067' 
   --[[ Checkout https://github.com/Josepdal/DBTeam
   --GNU GPL v2 license.]]
 end
 
 return {
   patterns = {
-    "^#version$"
+    "^#[Tt]ele[Ff]ox$"
   }, 
   run = run 
 }
